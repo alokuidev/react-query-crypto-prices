12e5f9aac466853c75b992d075c95345e9e11930
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-04-03 21:22:00 +0530

    query clinet implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import CryptoList from "./Component/CryptoList.jsx";
+import {QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
 function App() {
 
+ const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
         <CryptoList/>
-    </>
+    </QueryClientProvider>
   )
 }
 

ba0fd2ae432ff3d34bb2135c324e5117cd401899
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-04-03 21:14:26 +0530

    api call completed with error handling

---
+++ src/APi/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const CryptoApi = async () =>{
+    try {
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+        return response.data;
+    }
+    catch (e) {
+        console.log(`Error Occcured While Fetching Data:${e}`);
+        return [];
+    }
+}
+
+export default CryptoApi()
